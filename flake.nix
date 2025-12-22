0daa4286a65503b0771efa841ccd39b33c720a45
dewdgi <dewdgi@proton.me>
2025-12-22 15:16:02 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,31 +1,26 @@
 {
   description = "wheel-hid-emulator";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  outputs = { self, nixpkgs }: let
-    systems = [ "x86_64-linux" ];
-  in {
-    packages = builtins.listToAttrs (map (system: let pkgs = import nixpkgs { inherit system; }; in {
-      name = system;
-      value = pkgs.stdenv.mkDerivation {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let pkgsFor = system: import nixpkgs { inherit system; };
+    in {
+      packages.x86_64-linux.wheel-hid-emulator = (pkgsFor "x86_64-linux").stdenv.mkDerivation {
         pname = "wheel-hid-emulator";
         version = builtins.getEnv "GIT_COMMIT";
         src = ./.;
-        nativeBuildInputs = [ pkgs.pkg-config ];
-        buildInputs = [ pkgs.hidapi ];
+        nativeBuildInputs = [ (pkgsFor "x86_64-linux").pkg-config ];
+        buildInputs = [ (pkgsFor "x86_64-linux").hidapi ];
         buildPhase = "make";
         installPhase = ''
           mkdir -p $out/bin
           install -Dm755 wheel-emulator $out/bin/wheel-emulator
         '';
         meta = {
-          license = pkgs.lib.licenses.gpl2Only;
+          license = (pkgsFor "x86_64-linux").lib.licenses.gpl2Only;
           description = "wheel-hid-emulator";
         };
       };
-    }) systems);
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux;
-    devShells.x86_64-linux.default = import nixpkgs { system = "x86_64-linux"; }.mkShell { buildInputs = [ (import nixpkgs { system = "x86_64-linux"; }).pkg-config ]; };
-  };
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.wheel-hid-emulator;
+      devShells.x86_64-linux.default = (pkgsFor "x86_64-linux").mkShell { buildInputs = [ (pkgsFor "x86_64-linux").pkg-config ]; };
+    };
 }
